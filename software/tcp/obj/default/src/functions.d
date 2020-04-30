51e314ee6ff98c99ad83a0c7c4d57c7ee0a54388
tucker0410 <tucker.0410@gmail.com>
2020-04-30 11:12:58 -0700

    minor facelift
    added comments
    removed old comments (that weren't necessary)
    changed some file names, tested and successful run after
    added debug print statements

--- software/tcp/obj/default/src/functions.d
+++ software/tcp/obj/default/src/functions.d
@@ -1,4 +1,4 @@
-obj/default/src/stack.o: src/stack.c \
+obj/default/src/functions.o: src/functions.c \
  ../tcp_bsp//drivers/inc/altera_avalon_sgdma.h \
  ../tcp_bsp//HAL/inc/sys/alt_dev.h ../tcp_bsp/system.h \
  ../tcp_bsp/linker.h ../tcp_bsp//HAL/inc/sys/alt_llist.h \
@@ -7,12 +7,9 @@ obj/default/src/stack.o: src/stack.c \
  ../tcp_bsp//drivers/inc/altera_avalon_sgdma_descriptor.h \
  ../tcp_bsp//drivers/inc/altera_avalon_sgdma_regs.h \
  ../tcp_bsp//HAL/inc/io.h ../tcp_bsp//HAL/inc/alt_types.h \
- ../tcp_bsp//drivers/inc/altera_avalon_pio_regs.h \
- ../tcp_bsp//drivers/inc/altera_avalon_timer.h \
- ../tcp_bsp//HAL/inc/sys/alt_warning.h \
- ../tcp_bsp//drivers/inc/altera_avalon_timer_regs.h \
- src/../include/layer.h src/../include/stack.h \
- ../tcp_bsp//HAL/inc/sys/alt_stdio.h
+ ../tcp_bsp//drivers/inc/altera_avalon_pio_regs.h src/../include/stack.h \
+ ../tcp_bsp//HAL/inc/sys/alt_stdio.h ../tcp_bsp//HAL/inc/sys/alt_irq.h \
+ ../tcp_bsp//HAL/inc/nios2.h
 
 ../tcp_bsp//drivers/inc/altera_avalon_sgdma.h:
 
@@ -40,14 +37,10 @@ obj/default/src/stack.o: src/stack.c \
 
 ../tcp_bsp//drivers/inc/altera_avalon_pio_regs.h:
 
-../tcp_bsp//drivers/inc/altera_avalon_timer.h:
-
-../tcp_bsp//HAL/inc/sys/alt_warning.h:
-
-../tcp_bsp//drivers/inc/altera_avalon_timer_regs.h:
-
-src/../include/layer.h:
-
 src/../include/stack.h:
 
 ../tcp_bsp//HAL/inc/sys/alt_stdio.h:
+
+../tcp_bsp//HAL/inc/sys/alt_irq.h:
+
+../tcp_bsp//HAL/inc/nios2.h:
--- software/tcp/obj/default/src/main.d
+++ software/tcp/obj/default/src/main.d
@@ -1,4 +1,4 @@
-obj/default/src/layer.o: src/layer.c \
+obj/default/src/main.o: src/main.c \
  ../tcp_bsp//drivers/inc/altera_avalon_sgdma.h \
  ../tcp_bsp//HAL/inc/sys/alt_dev.h ../tcp_bsp/system.h \
  ../tcp_bsp/linker.h ../tcp_bsp//HAL/inc/sys/alt_llist.h \
@@ -7,9 +7,12 @@ obj/default/src/layer.o: src/layer.c \
  ../tcp_bsp//drivers/inc/altera_avalon_sgdma_descriptor.h \
  ../tcp_bsp//drivers/inc/altera_avalon_sgdma_regs.h \
  ../tcp_bsp//HAL/inc/io.h ../tcp_bsp//HAL/inc/alt_types.h \
- ../tcp_bsp//drivers/inc/altera_avalon_pio_regs.h src/../include/stack.h \
- ../tcp_bsp//HAL/inc/sys/alt_stdio.h ../tcp_bsp//HAL/inc/sys/alt_irq.h \
- ../tcp_bsp//HAL/inc/nios2.h
+ ../tcp_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../tcp_bsp//drivers/inc/altera_avalon_timer.h \
+ ../tcp_bsp//HAL/inc/sys/alt_warning.h \
+ ../tcp_bsp//drivers/inc/altera_avalon_timer_regs.h \
+ src/../include/layer.h src/../include/stack.h \
+ ../tcp_bsp//HAL/inc/sys/alt_stdio.h
 
 ../tcp_bsp//drivers/inc/altera_avalon_sgdma.h:
 
@@ -37,10 +40,14 @@ obj/default/src/layer.o: src/layer.c \
 
 ../tcp_bsp//drivers/inc/altera_avalon_pio_regs.h:
 
+../tcp_bsp//drivers/inc/altera_avalon_timer.h:
+
+../tcp_bsp//HAL/inc/sys/alt_warning.h:
+
+../tcp_bsp//drivers/inc/altera_avalon_timer_regs.h:
+
+src/../include/layer.h:
+
 src/../include/stack.h:
 
 ../tcp_bsp//HAL/inc/sys/alt_stdio.h:
-
-../tcp_bsp//HAL/inc/sys/alt_irq.h:
-
-../tcp_bsp//HAL/inc/nios2.h:
